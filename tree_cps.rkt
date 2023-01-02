c73dabc3ecc8a1992e04c3aa1e2f8f88d84d3b26
WhiteCat6142 <whitecat6142+git@gmail.com>
2023-01-02 23:28:05 +0900

    cps

--- tree_cps.rkt
+++ tree_cps.rkt
@@ -1,6 +1,8 @@
 #lang racket
 ;; https://practical-scheme.net/wiliki/wiliki.cgi?Scheme%3A%E9%83%A8%E5%88%86%E7%B6%99%E7%B6%9A%3A%E3%82%A4%E3%83%86%E3%83%AC%E3%83%BC%E3%82%BF%E3%81%AE%E5%8F%8D%E8%BB%A2
 ;; https://practical-scheme.net/wiliki/wiliki.cgi?Scheme%3A%E3%81%AA%E3%81%9CScheme%E3%81%AB%E3%81%AFreturn%E3%81%8C%E7%84%A1%E3%81%84%E3%81%AE%E3%81%8B
+;; https://practical-scheme.net/wiliki/wiliki.cgi?Scheme%3A%E4%BD%BF%E3%81%84%E3%81%9F%E3%81%84%E4%BA%BA%E3%81%AE%E3%81%9F%E3%82%81%E3%81%AE%E7%B6%99%E7%B6%9A%E5%85%A5%E9%96%80
+;; http://www.nct9.ne.jp/m_hiroi/func/abcscm20.html
 (require racket/control)
 (require graphics/turtles)
 (turtles)
